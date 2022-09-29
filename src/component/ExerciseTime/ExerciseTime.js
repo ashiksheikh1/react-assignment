0ce013faf7f94ea17ee000e90dafe56a3ba8ff8a
ashiksheikh1 <ashiksheikh.web13@gmail.com>
2022-09-29 20:46:38 +0600

    colornew

--- src/component/ExerciseTime/ExerciseTime.js
+++ src/component/ExerciseTime/ExerciseTime.js
@@ -11,22 +11,25 @@ const ExerciseTime = ({ cart }) => {
 
     const breakTime = (time) => {
         setTime(time)
+        localStorage.setItem("time", time)
     }
+    const getTime = localStorage.getItem('time')
     return (
         <div>
             <h4>Add A Break</h4>
             <div className='Break-btn'>
-                <button onClick={() => breakTime(10)}>10</button>
-                <button onClick={() => breakTime(20)}>20</button>
-                <button onClick={() => breakTime(30)}>30</button>
-                <button onClick={() => breakTime(40)}>40</button>
+                <button onClick={() => breakTime(10)}>10s</button>
+                <button onClick={() => breakTime(20)}>20s</button>
+                <button onClick={() => breakTime(30)}>30s</button>
+                <button onClick={() => breakTime(40)}>40s</button>
             </div>
             <div className="Exercise">
                 <h4>Exercise Details:{total}s</h4>
             </div>
 
             <div className="Exercise">
-                <h4>Break time:{time}s</h4>
+                <h4>Break time:{getTime}s</h4>
+
             </div>
         </div>
     );
